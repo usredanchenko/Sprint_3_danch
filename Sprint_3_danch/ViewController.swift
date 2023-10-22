5769e061778eaef41d3b3e5be4d59acbd85b8eac
sredanchenko <danchenko43@gmail.com>
2023-10-22 17:56:36 +0300

    Первый коммит

--- Sprint_3_danch/ViewController.swift
+++ Sprint_3_danch/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController:UIViewController{
+    
+    @IBOutlet weak var mistakeError: UITextView!
+    @IBOutlet weak var schetNum: UILabel!
+    var i = 0
+    var b = 0
+    мфк с = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        }
+    @IBAction func plusButt(_ sender: Any) {
+        
+            i = i + 1
+            schetNum.text = "\(i)"
+       
     }
-
-
+    @IBAction func minusButt2(_ sender: Any) {
+        if i > 0{
+            i = i - 1
+            schetNum.text = "\(i)"
+        }
+        else{
+    
+        }
+    }
 }
-
