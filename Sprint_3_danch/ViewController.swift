d48517fc4cc31b4859926c03d5e505a8749e5ab9
sredanchenko <danchenko43@gmail.com>
2023-10-22 14:30:58 +0300

    Initial Commit

---
+++ Sprint_3_danch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sprint_3_danch
+//
+//  Created by e.s.danchenko on 22.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sprint_3_danchUITests/Sprint_3_danchUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Sprint_3_danchUITestsLaunchTests.swift
+//  Sprint_3_danchUITests
+//
+//  Created by e.s.danchenko on 22.10.2023.
+//
+
+import XCTest
+
+final class Sprint_3_danchUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
