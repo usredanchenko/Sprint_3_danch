abc21f83357073f75ff1fad3d392222cc8c266eb
sredanchenko <danchenko43@gmail.com>
2023-10-23 21:30:17 +0300

    finalCommit

--- Sprint_3_danch/ViewController.swift
+++ Sprint_3_danch/ViewController.swift
@@ -6,30 +6,50 @@
 //
 
 import UIKit
+import Foundation
+
+let todayDate = Date()
 
 class ViewController:UIViewController{
     
-    @IBOutlet weak var mistakeError: UITextView!
-    @IBOutlet weak var schetNum: UILabel!
-    var i = 0
-    var b = 0
-    мфк с = 0
+    @IBOutlet weak var lableNum: UILabel!
+    @IBOutlet weak var dateAler: UILabel!
+     var i = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         }
     @IBAction func plusButt(_ sender: Any) {
-        
+        if i >= 0{
             i = i + 1
-            schetNum.text = "\(i)"
-       
+            lableNum.text = "\(i)"
+            dateAler.text = nil
+        }
+        else {
+            i = i + 1
+            lableNum.text = "\(0)"
+            dateAler.text = "\(todayDate) Занчение сброшено"
+        }
     }
-    @IBAction func minusButt2(_ sender: Any) {
-        if i > 0{
-            i = i - 1
-            schetNum.text = "\(i)"
-        }
-        else{
     
+   
+    @IBAction func minusButt2(_ sender: Any) {
+        if i == 0 {
+            i = i - 1
+            dateAler.text = "\(todayDate) Занчение сброшено"
+            lableNum.text = "\(0)"
         }
+        else if i < 0 {
+            i = i - 1
+            dateAler.text = "\(todayDate) попытка уменьшить значение счётчика ниже 0"
+            lableNum.text = "\(0)"
+        }
+        else {
+            i = i - 1
+            lableNum.text = "\(i)"
+            dateAler.text = "\(todayDate) Занчение сброшено"
+            return
+        }
+        
     }
 }
